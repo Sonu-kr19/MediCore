e0d26e476c0800d3469ad92158c21db05ed3019e
Sonu <Sonu.Kumar6@cognizant.com>
2026-03-25 22:31:55 +0530

    MCP-56: Configure JWT Authentication Middleware

--- MediCore.Api/Program.cs
+++ MediCore.Api/Program.cs
@@ -1,9 +1,12 @@
+using System.Text;
 using MediCore.Api.Repositories;
 using MediCore.Api.Repositories.UserRepo;
 using MediCore.Api.Services;
 using MediCore.Api.Services.AuthServices;
 using MediCore.Domain.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +19,34 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<MediCoreDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
         b => b.MigrationsAssembly("MediCore.Api")));
-        
+
+//Read JWT Configuration
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+
+// Add Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme=JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme=JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters=new TokenValidationParameters
+    {
+        ValidateIssuer=true,
+        ValidateAudience=true,
+        ValidateLifetime=true,
+        ValidateIssuerSigningKey=true,
+
+        ValidIssuer=jwtSettings["Issuer"],
+        ValidAudience=jwtSettings["Audience"],
+        IssuerSigningKey=new SymmetricSecurityKey(key),
+        ClockSkew=TimeSpan.Zero
+    };
+});
+// Added Authorization
+builder.Services.AddAuthorization();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -27,8 +57,14 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+//Middleware Pipeline
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseHttpsRedirection();
 app.Run();
 
