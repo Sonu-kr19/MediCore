f0dfb1751b767158260d2982af1394de0a70429f
Goutham <gouthamraa@gmail.com>
2026-04-09 14:53:06 +0530

    Corrected spaces

--- MediCore.Api/Controllers/PharmacyController.cs
+++ MediCore.Api/Controllers/PharmacyController.cs
@@ -16,23 +16,19 @@ namespace MediCore.Api.Controllers
             _prescriptionService = prescriptionService;
         }
 
-
         [HttpGet("queue")]
         public async Task<IActionResult> GetQueuedPrescriptions(
             int pageNumber,
             int pageSize)
         {
-
             if (pageNumber < 1)
             {
                 throw new ArgumentException("pageNumber must be greater than or equal to 1.");
             }
-
             if (pageSize < 1)
             {
                 throw new ArgumentException("pageSize must be greater than or equal to 1.");
             }
-            
             var result =
                 await _prescriptionService
                     .GetQueuedPrescriptionsAsync(pageNumber, pageSize);
