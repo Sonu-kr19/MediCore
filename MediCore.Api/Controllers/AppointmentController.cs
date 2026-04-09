a1c5f10631cbd49a33e076e066b5196a570107f3
Sonu <Sonu.Kumar6@cognizant.com>
2026-04-09 16:47:55 +0530

    MCP-132: Getting wrong status codes for Missing doctorId parameter, Missing date parameter. And the error message is not printing in response body for Invalid doctor Id

--- MediCore.Api/Controllers/AppointmentController.cs
+++ MediCore.Api/Controllers/AppointmentController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MediCore.Api.Controllers
 {
-    // [Authorize]
+    [Authorize]
     [Route("api/v1/[controller]")]
     [ApiController]
     public class AppointmentController : ControllerBase
