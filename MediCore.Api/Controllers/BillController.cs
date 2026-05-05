fb4e6daf22633c1fdc0c87437f13f374c7489296
Saikumar <saikumarvidiyala81@gmail.com>
2026-05-05 12:56:59 +0530

    Edited the route in billcontroller

--- MediCore.Api/Controllers/BillController.cs
+++ MediCore.Api/Controllers/BillController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MediCore.Api.Controllers
 {
-    [Route("api/[controller]/billing")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class BillController : ControllerBase
     {
